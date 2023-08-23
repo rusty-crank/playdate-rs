fdd37927e1ef5e76162a23d01e48c577cd7b1b2a
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2023-08-23 20:04:56 +1000

    fix build.rs

--- playdate-rs-sys/build.rs
+++ playdate-rs-sys/build.rs
@@ -7,10 +7,6 @@ fn main() {
     // Tell cargo to invalidate the built crate whenever the wrapper changes
     println!("cargo:rerun-if-changed=wrapper.h");
 
-    if cfg!(not(all(target_arch = "arm", target_os = "none"))) {
-        return;
-    }
-
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindgen_helper::generate(false, out_path.join("bindings.rs"), None);
 }
