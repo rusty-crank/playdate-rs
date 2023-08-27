a55d1c959570a35ee23946d9f41640b9e3549892
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2023-08-27 15:10:08 +1000

    Update publish scripts

--- .github/scripts/publish.sh
+++ .github/scripts/publish.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
+STATUS="$(cmp --silent README.md playdate-rs/README.md; echo $?)"  # "$?" gives exit status for each comparison
+
+if [[ $STATUS -ne 0 ]]; then  # if status isn't equal to 0, then execute code
+    echo "ERROR: README.md and playdate-rs/README.md are not identical!"
+    exit -1
+fi
+
 set -ex
 
-cargo publish -p playdate-rs-sys
-cargo publish -p playdate-rs-macros
-cargo publish -p playdate-rs
-cargo publish -p playdate-cli
+cargo publish -p playdate-rs-sys $@
+cargo publish -p playdate-rs-macros $@
+cargo publish -p playdate-rs $@
+cargo publish -p playdate-cli $@
 
